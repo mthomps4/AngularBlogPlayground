a6873f83d6fd3f3ae52b13d75b76b65919cf003e
Matthew <matthew.thompson.a@gmail.com>
2016-07-23 22:25:32 -0400

    playground testing.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// https://codepen.io/nickmoreton/pen/mgtLK
+
 var blogApp = angular.module("blogApp", []);
 
 blogApp.controller('title', function($scope){
@@ -6,18 +8,10 @@ blogApp.controller('title', function($scope){
 });
 
 blogApp.controller('BlogPosts', function($scope, $http){
-  $http.get('Posts.JSON').success(function(data){
+  $http.get('Posts.json').success(function(data){
+
      $scope.posts = {};
      $scope.posts = data;
      console.log($scope.posts);
    });
-});
-
-blogApp.controller('thisFullPost', function($scope,$http){
-  //Prevent A link Default path.
-  //Provide Template URL with link back to ALL POSTS
-  $scope.this.addEventListener("click", function(){
-    alert($scope.post);
-  });
-
 });
